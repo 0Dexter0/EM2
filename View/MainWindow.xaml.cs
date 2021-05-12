259c605e3bab20cb9a518cc174b8495c65688c49
0Dexter0 <bsv16082002@gmail.com>
2021-05-12 21:54:43 +0300

    Update interface

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -21,24 +21,28 @@ namespace EM2.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int EnterPressed { get; set; } = 1;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void MI_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            MIWindows.Background = Brushes.LightGray;
-        }
-
         private void MI_MouseLeave(object sender, MouseEventArgs e)
         {
-            MIWindows.Background = Brushes.White;
+            SolidColorBrush solidColor = new();
+            solidColor.Color = new Color() { R = 20, G = 20, B = 20 };
+            MIWindows.Background = solidColor;
+            MIRegisters.Background = solidColor;
         }
 
         private void MI_MouseEnter(object sender, MouseEventArgs e)
         {
-            MIWindows.Background = Brushes.LightGray;
+            SolidColorBrush solidColor = new();
+            solidColor.Color = new Color() { R = 20, G = 20, B = 20 };
+            MIWindows.Background = solidColor;
+            MIRegisters.Background = solidColor;
+            MIRegisters.Foreground = Brushes.Black;
         }
 
         private void TBInput_KeyDown(object sender, KeyEventArgs e)
@@ -46,12 +50,46 @@ namespace EM2.View
             if (e.KeyboardDevice.Modifiers == ModifierKeys.Control && e.Key == Key.S)
             {
                 TBInput.IsEnabled = false;
-                //TODO: Create Save window
-                Save savePage = new();
 
-                savePage.Owner = this;
-                //savePage.Show();
+                TimerCallback callback = new(ClearTBInfo);
+                Timer timer = new(callback, null, 2000, 0);
+
+                TBInfo.Text = "Saved";
+               
                 TBInput.IsEnabled = true;
+            }
+            if (e.Key == Key.Return)
+            {
+                
+
+                EnterPressed++;
+                TBInfo.Text = EnterPressed.ToString();
+            }
+        }
+
+        private void ClearTBInfo(object o)
+        {
+            Dispatcher.Invoke(() => TBInfo.Text = "");
+        }
+
+        private void TBInput_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TBInput.BorderBrush = Brushes.Black;
+        }
+
+        private void TBInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                EnterPressed++;
+
+                SPIndex.Children.Add(new TextBlock()
+                {
+                    Text = EnterPressed.ToString(),
+                    Padding = new Thickness(8, 0, 0, 0),
+                    Foreground = Brushes.White,
+                    FontSize = 15
+                });
             }
         }
     }
